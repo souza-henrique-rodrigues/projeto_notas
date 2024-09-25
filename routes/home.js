caaafd4a3edd4eb36caa183cece8a4982e98e630
Henrique Rodrigues <tumbrl666@gmail.com>
2024-09-25 18:27:22 -0300

    add raw layout

--- routes/home.js
+++ routes/home.js
@@ -4,13 +4,17 @@ const router = express.Router();
 texto = [];
 
 router.get("/home", (req, res, next) => {
-  res.render("home");
+  res.render("home", { pageTitle: "adicionar nota" });
 });
 
 router.post("/home", (req, res, next) => {
-  texto.push({ nomeNota: req.body.addNota });
-  res.redirect("/home");
-  console.log(texto);
+  if (req.body.addNota == "") {
+    res.redirect("/home");
+  } else {
+    texto.push({ nomeNota: req.body.addNota });
+    res.redirect("/home");
+    console.log(texto);
+  }
 });
 
 module.exports = router;
